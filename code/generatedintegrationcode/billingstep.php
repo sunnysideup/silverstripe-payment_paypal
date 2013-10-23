4a8838d0a2eca5a50bf3f71b813e31f11390ace8
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-10-23 01:17:38 +0000

    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_paypal/trunk@4829 950b781c-c544-efab-2f99-90083ee69683

--- code/generatedintegrationcode/billingstep.php
+++ code/generatedintegrationcode/billingstep.php
@@ -9,18 +9,18 @@ if ( $PaymentOption == "PayPal")
 	// ==================================
 
 	//'------------------------------------
-	//' The paymentAmount is the total value of 
-	//' the shopping cart, that was set 
-	//' earlier in a session variable 
+	//' The paymentAmount is the total value of
+	//' the shopping cart, that was set
+	//' earlier in a session variable
 	//' by the shopping cart page
 	//'------------------------------------
 	$paymentAmount = $_SESSION["Payment_Amount"];
 
 	//'------------------------------------
-	//' When you integrate this code 
-	//' set the variables below with 
-	//' shipping address details 
-	//' entered by the user on the 
+	//' When you integrate this code
+	//' set the variables below with
+	//' shipping address details
+	//' entered by the user on the
 	//' Shipping page.
 	//'------------------------------------
 	$shipToName = "<<ShiptoName>>";
@@ -33,8 +33,8 @@ if ( $PaymentOption == "PayPal")
 	$phoneNum = "<<PhoneNumber>>";
 
 	//'------------------------------------
-	//' The currencyCodeType and paymentType 
-	//' are set to the selections made on the Integration Assistant 
+	//' The currencyCodeType and paymentType
+	//' are set to the selections made on the Integration Assistant
 	//'------------------------------------
 	$currencyCodeType = "NZD";
 	$paymentType = "Sale";
@@ -43,7 +43,7 @@ if ( $PaymentOption == "PayPal")
 	//' The returnURL is the location where buyers return to when a
 	//' payment has been succesfully authorized.
 	//'
-	//' This is set to the value entered on the Integration Assistant 
+	//' This is set to the value entered on the Integration Assistant
 	//'------------------------------------
 	$returnURL = "return";
 
@@ -51,7 +51,7 @@ if ( $PaymentOption == "PayPal")
 	//' The cancelURL is the location buyers are sent to when they hit the
 	//' cancel button during authorization of payment during the PayPal flow
 	//'
-	//' This is set to the value entered on the Integration Assistant 
+	//' This is set to the value entered on the Integration Assistant
 	//'------------------------------------
 	$cancelURL = "cancel";
 
@@ -72,15 +72,15 @@ if ( $PaymentOption == "PayPal")
 		$token = urldecode($resArray["TOKEN"]);
 		$_SESSION['reshash']=$token;
 		RedirectToPayPal ( $token );
-	} 
-	else  
+	}
+	else
 	{
 		//Display a user friendly Error on the page using any of the following error information returned by PayPal
 		$ErrorCode = urldecode($resArray["L_ERRORCODE0"]);
 		$ErrorShortMsg = urldecode($resArray["L_SHORTMESSAGE0"]);
 		$ErrorLongMsg = urldecode($resArray["L_LONGMESSAGE0"]);
 		$ErrorSeverityCode = urldecode($resArray["L_SEVERITYCODE0"]);
-		
+
 		echo "SetExpressCheckout API call failed. ";
 		echo "Detailed Error Message: " . $ErrorLongMsg;
 		echo "Short Error Message: " . $ErrorShortMsg;
@@ -94,53 +94,53 @@ else
 			&& ( $PaymentProcessorSelected == "PayPal Direct Payment"))
 
 	//'------------------------------------
-	//' The paymentAmount is the total value of 
-	//' the shopping cart, that was set 
-	//' earlier in a session variable 
+	//' The paymentAmount is the total value of
+	//' the shopping cart, that was set
+	//' earlier in a session variable
 	//' by the shopping cart page
 	//'------------------------------------
 	$paymentAmount = $_SESSION["Payment_Amount"];
 
 	//'------------------------------------
-	//' The currencyCodeType and paymentType 
-	//' are set to the selections made on the Integration Assistant 
+	//' The currencyCodeType and paymentType
+	//' are set to the selections made on the Integration Assistant
 	//'------------------------------------
 	$currencyCodeType = "NZD";
 	$paymentType = "Sale";
-	
+
 	//' Set these values based on what was selected by the user on the Billing page Html form
-	
+
 	$creditCardType 		= "<<Visa/MasterCard/Amex/Discover>>"; //' Set this to one of the acceptable values (Visa/MasterCard/Amex/Discover) match it to what was selected on your Billing page
 	$creditCardNumber 		= "<<CC number>>"; //' Set this to the string entered as the credit card number on the Billing page
 	$expDate 				= "<<Expiry Date>>"; //' Set this to the credit card expiry date entered on the Billing page
-	$cvv2 					= "<<cvv2>>"; //' Set this to the CVV2 string entered on the Billing page 
-	$firstName 				= "<<firstName>>"; //' Set this to the customer's first name that was entered on the Billing page 
-	$lastName 				= "<<lastName>>"; //' Set this to the customer's last name that was entered on the Billing page 
-	$street 				= "<<street>>"; //' Set this to the customer's street address that was entered on the Billing page 
-	$city 					= "<<city>>"; //' Set this to the customer's city that was entered on the Billing page 
-	$state 					= "<<state>>"; //' Set this to the customer's state that was entered on the Billing page 
-	$zip 					= "<<zip>>"; //' Set this to the zip code of the customer's address that was entered on the Billing page 
-	$countryCode 			= "<<PayPal Country Code>>"; //' Set this to the PayPal code for the Country of the customer's address that was entered on the Billing page 
-	$currencyCode 			= "<<PayPal Currency Code>>"; //' Set this to the PayPal code for the Currency used by the customer 
-	
-	/*	
+	$cvv2 					= "<<cvv2>>"; //' Set this to the CVV2 string entered on the Billing page
+	$firstName 				= "<<firstName>>"; //' Set this to the customer's first name that was entered on the Billing page
+	$lastName 				= "<<lastName>>"; //' Set this to the customer's last name that was entered on the Billing page
+	$street 				= "<<street>>"; //' Set this to the customer's street address that was entered on the Billing page
+	$city 					= "<<city>>"; //' Set this to the customer's city that was entered on the Billing page
+	$state 					= "<<state>>"; //' Set this to the customer's state that was entered on the Billing page
+	$zip 					= "<<zip>>"; //' Set this to the zip code of the customer's address that was entered on the Billing page
+	$countryCode 			= "<<PayPal Country Code>>"; //' Set this to the PayPal code for the Country of the customer's address that was entered on the Billing page
+	$currencyCode 			= "<<PayPal Currency Code>>"; //' Set this to the PayPal code for the Currency used by the customer
+
+	/*
 	'------------------------------------------------
 	' Calls the DoDirectPayment API call
 	'
 	' The DirectPayment function is defined in PayPalFunctions.php included at the top of this file.
 	'-------------------------------------------------
 	*/
-	
+
 	$resArray = DirectPayment ( $paymentType, $paymentAmount, $creditCardType, $creditCardNumber,
-							$expDate, $cvv2, $firstName, $lastName, $street, $city, $state, $zip, 
-							$countryCode, $currencyCode ); 
+							$expDate, $cvv2, $firstName, $lastName, $street, $city, $state, $zip,
+							$countryCode, $currencyCode );
 
 	$ack = strtoupper($resArray["ACK"]);
 	if($ack=="SUCCESS" || $ack=="SUCCESSWITHWARNING")
 	{
-		//Getting transaction ID from API responce. 
-		$TransactionID = urldecode9$resArray["TRANSACTIONID"]);
-		
+		//Getting transaction ID from API responce.
+		$TransactionID = urldecode($resArray["TRANSACTIONID"]);
+
 		echo "Your payment has been successfully processed";
 	}
 	else
@@ -150,7 +150,7 @@ else
 		$ErrorShortMsg = urldecode($resArray["L_SHORTMESSAGE0"]);
 		$ErrorLongMsg = urldecode($resArray["L_LONGMESSAGE0"]);
 		$ErrorSeverityCode = urldecode($resArray["L_SEVERITYCODE0"]);
-		
+
 		echo "Direct credit card payment API call failed. ";
 		echo "Detailed Error Message: " . $ErrorLongMsg;
 		echo "Short Error Message: " . $ErrorShortMsg;
